fef4ab20fd110c65108f5a11a96dab55a60b2ca3
nishakar123 <nishakar123@gmail.com>
2016-12-20 15:44:15 +0530

    add sop c

--- src/A.java
+++ src/A.java
@@ -4,6 +4,7 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("A");
 		System.out.println("B");
+		System.out.println("C");
 		System.out.println("D");
 		System.out.println("E");
 	}
